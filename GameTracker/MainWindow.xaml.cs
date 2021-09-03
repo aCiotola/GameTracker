2e47e7b46708cdb6dcca1a4067a512ed5077559f
aCiotola <alessandromciotola@gmail.com>
2021-09-03 10:57:55 -0400

    Initial Design, RestSharp Implementation
    Very basic design (mainly for remembering the layout I envisioned)
    Got an API key from RAWG (should be saved to a file so no other user can see it (.gitignore))

--- GameTracker/MainWindow.xaml.cs
+++ GameTracker/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GameTracker.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace GameTracker
         public MainWindow()
         {
             InitializeComponent();
+            RestSharpUtil rs = new RestSharpUtil();
         }
     }
 }
---
+++ GameTracker/Utilities/RestSharpUtil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using RestSharp;
+using System.Text.Json;
+using RestSharp.Authenticators;
+
+namespace GameTracker.Utilities
+{
+    class RestSharpUtil
+    {
+        public RestSharpUtil() {
+
+            var client = new RestClient("https://rawg-video-games-database.p.rapidapi.com/platforms/1?key=db013a96fb7c45cb9b012e6a625d618e");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-host", "rawg-video-games-database.p.rapidapi.com");
+            request.AddHeader("x-rapidapi-key", "7e40c52d95mshfe4b8e96d2705a8p1e477djsnfedab9b3a5a9");
+            IRestResponse response = client.Execute(request);
+
+            MessageBox.Show(response.Content);
+
+            //MAKE CLASS WHICH TAKES ALL JSON VALUES
+        }
+
+
+    }
+}
